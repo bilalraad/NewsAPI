1556e0c8ee1b191b36dca009c6062a14c335ef48
Bilal raad <bilal52040@gmail.com>
2024-07-14 21:48:27 +0300

    refactor: :art: seperate prod from dev container

--- obj/Debug/net8.0/NewsAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/NewsAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f53e60205cad419d1bb566dc18a8234181eaf418")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3efae905ad8ec64cb6e82826bfb81a873f4a325f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
