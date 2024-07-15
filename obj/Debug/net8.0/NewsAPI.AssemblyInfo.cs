ab0d04a9074fa925e0a05ff1b9171bd80317ecb7
Bilal raad <bilal52040@gmail.com>
2024-07-16 00:08:02 +0300

    refactor: :memo:

--- obj/Debug/net8.0/NewsAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/NewsAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1693c439a1d69c39d2bb94ded3fa2754d9c5b50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae553963636d61101de717f33fef3481e00b5303")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
